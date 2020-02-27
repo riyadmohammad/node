b1c55998b2d8feb8835e37345fd55811cf605a7a
riyadmohammad <riyad.muhammad47@gmai47.com>
2020-02-27 11:40:08 +0600

    adding

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+
+var home = require('./controllers/home');
+
+//var fileUplod = require('express-fileupload');
+
+
+
+
+
+
+var app=express();
+
+
+app.set('view engine', 'ejs');
+
+app.use('/home', home);
+
+
+
+
+
+app.use(cookieParser());
+//app.use(fileUpload());
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+
+
+
+app.get('/',function(req,res){
+	
+	res.send('welcome');
+	
+});
+
+
+app.listen(3000,function(){
+	
+	console.log('node server started at 3000!');
+	
+});
---
+++ controllers/home.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+//var userModel = require.main.require('./models/user-model');
+
+router.get('/', function(req, res){
+	
+		res.render('home/index');
+
+});
+
+router.post('/signup', function(req, res){
+	
+		res.render('reg/index');
+
+});
+
+
+
+module.exports = router;
